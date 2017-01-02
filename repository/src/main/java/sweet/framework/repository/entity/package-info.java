c665bcb248b1c21cb3f2771f34d54e21142afeb3
sweet <sweet123951@gmail.com>
2017-01-02 19:59:19 +0800

    modify

--- repository/src/main/java/sweet/framework/repository/entity/package-info.java
+++
@@ -1,1 +0,0 @@
-package sweet.framework.repository.entity;
---
+++ repository/src/main/java/sweet/framework/repository/pojo/User.java
@@ -0,0 +1,66 @@
+package sweet.framework.repository.pojo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private BigDecimal salary;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+}
